61e939cec1a3d12916481c8215457906372e6188
Tiffany Marroquin <tiffany.marroq@gmail.com>
2020-01-08 13:22:07 -0500

    adding in side drawer

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+const NavLinks = props => {
+    return (
+        <ul className='nav-links'>
+            <li>
+                <NavLink to='/' exact>ALL USERS</NavLink>
+            </li>
+            <li>
+                <NavLink to='/ul/places'>ALL PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to='/places/new'>ADD PLACE</NavLink>
+            </li>
+            <li>
+                <NavLink to='/auth'>AUTH</NavLink>
+            </li>
+        </ul>
+    )
+
+}
+
+export default NavLinks;
