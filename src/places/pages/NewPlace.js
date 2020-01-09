53d5e1a8df96d309ffa187642107711338655869
Tiffany Marroquin <tiffany.marroq@gmail.com>
2020-01-09 12:57:38 -0500

    setting up input setting

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/FormElements/Input';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../utils/validators';
 import './NewPlace.css';
 
 
+
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    }, [])
+
+    const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+    }, [])
+
     return (
         <form className="place-form">
-            <Input element="input" type="text" label="Title" validators = {[]} errorText="Please enter text"   />
+            <Input 
+                id='title'
+                element="input" 
+                type="text" 
+                label="Title" 
+                onInput={titleInputHandler}
+                validators = {[VALIDATOR_REQUIRE()]} 
+                errorText="Please enter text"   />
+            <Input 
+                id='description'
+                element="textarea" 
+                type="text" 
+                label="Description" 
+                onInput={titleInputHandler}
+                validators = {[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]} 
+                errorText="Please enter valid description"   />
         </form>
     )
 };
--- src/shared/FormElements/Input.js
+++ src/shared/FormElements/Input.js
@@ -1,5 +1,5 @@
-import React, { useReducer } from 'react';
-
+import React, { useReducer, useEffect } from 'react';
+import { validate } from '../../utils/validators';
 import './Input.css';
 
 const inputReducer = (state, action) => {
@@ -8,8 +8,13 @@ const inputReducer = (state, action) => {
             return {
                 ...state,
                 value: action.val,
-                isValid: true
+                isValid: validate(action.val, action.validators)
             }
+        case 'TOUCH':
+                return {
+                    ...state,
+                    isTouched: true,
+                }
         default:
             return state;
     }
@@ -18,11 +23,30 @@ const inputReducer = (state, action) => {
 
 const Input = props => {
 
-    const [inputState, dispatch] = useReducer(inputReducer, {value: '', isValid: false});
+    const [ inputState, dispatch ] = useReducer(
+        inputReducer, {
+        value: '', 
+        isValid: false,
+        isTouched: false,
+        
+    });
+
+    const { id, onInput } = props; 
+    const { value, isValid } = inputState;
+
+    useEffect( () => {
+        onInput(id, value, isValid)
+    }, [ id, value, isValid, onInput ])
 
     const changeHandler = event => {
         dispatch({
-            type: 'CHANGE', val: event.target.value
+            type: 'CHANGE', val: event.target.value, validators: props.validators
+        })
+    }
+
+    const touchHandler = () => {
+        dispatch({
+            type: 'TOUCH'
         })
     }
 
@@ -33,23 +57,24 @@ const Input = props => {
             ype={props.type} 
             placeholder={props.placeholder} 
             onChange={changeHandler}
+            onBlur={touchHandler}
             value={inputState.value}
         />
         ) : (
         <textarea 
             id={props.id} 
             rows={props.rows || 3} 
+            onBlur={touchHandler}
             onChange={changeHandler} 
             value={inputState.value}  />
         );
         
-    
 
     return (
-        <div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
+        <div className={`form-control ${!inputState.isValid && inputState.isTouched && 'form-control--invalid'  }`}>
         <label htmlFor={props.id}>{props.label}</label>
         {element}
-        {!inputState.isValid && <p>{props.errorText}</p>}
+        {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
         </div>
     );
 };
